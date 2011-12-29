2b7c7876d56b4a78c76150a7186a72c8f48ecc39
ksmt305 <ksmt305@gmail.com>
2011-12-28 20:28:42 -0800

    implemented Bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HelloRails::Application.routes.draw do
+  resources :bookmarks
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ HelloRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'bookmarks#index'
 
   # See how all your routes lay out with "rake routes"
 
